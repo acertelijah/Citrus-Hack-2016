c23b7c212115f91d31a5050a51d57bd6edbf3712
jgonz01 <jgonz117@ucr.edu>
2016-10-22 19:49:35 -0700

    Menu script

---
+++ MenuCtrl.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuCtrl : MonoBehaviour 
+{
+	public void LoadScene(string sceneName)
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+}
